d1d8d23a6b4b4d1ac1b22f88b437fdebf1c667cb
pAsta-kun <112457969+pAsta-kun@users.noreply.github.com>
2023-03-26 12:18:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function query(data) {
 	const response = await fetch(
 		"https://api-inference.huggingface.co/models/bigscience/bloom",
 		{
-			headers: { Authorization: "Bearer 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" },
+			headers: { Authorization: "Bearer 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" },
 			method: "POST",
 			body: JSON.stringify(data),
 		}
@@ -12,38 +12,47 @@ async function query(data) {
 }
 
 query({"inputs": "Can you please let us know more details about your "}).then((response) => {
-	console.log(response["generated_text"][0]);
-
+	console.log(JSON.stringify(response));
 });
 
+//stuff to do with text bubbles ig
+let newUserArea;
+let newUserBubble;
+let aiResponse; 
+
+//actrion listener for if user inputted stuff
 document.querySelector('#text').addEventListener('keypress', function (e) {
 	if(e.key == 'Enter')
 	{
-		
 		userEnter(document.getElementById('text').value);
-
-
 		gift()
-
 	}
 })
-
+//Creates new user text bubble
 function userEnter(text)
 {
-	document.createElement('div').classList.add('userArea');
+	//Creating new user text bubble
+	newUserArea = document.createElement('div');
+	newUserBubble = document.createElement('div');
+	newUserArea.classList.add('userArea');
+	newUserBubble.classList.add('userBubble', 'textBubble');
+	newUserArea.appendChild(newUserBubble);
+	//setting text of user textbubble
+	newUserBubble.innerHTML = text;
+	document.getElementsByClassName('textArea')[0].appendChild(newUserArea)
+	//Reseting textbox
+	document.getElementById('text').value = null;
 
-	//console.log('enter')
-	console.log(text);
-	query(document.getElementById('text').value);
+	query(text);
+	
 }
-
 function gift()
 {
-			// useless -- NO 
-		let randomNum  = Math.floor((Math.random()*(100)))
-		console.log(randomNum)
+	// useless -- NO 
+	let randomNum = Math.floor((Math.random()*(1000)))
 
-		//Gift from sammy wu
-		if( randomNum === 69)
-			window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	//Gift from sammy wu
+	if( randomNum === 69)
+		window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
