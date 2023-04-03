9723055ab25581d06ca657d81463e2e8d25b8524
pAsta-kun <112457969+pAsta-kun@users.noreply.github.com>
2023-04-02 21:12:04 -0500

    fixing front end stuff -- still need to fix div scroll

--- client/index.js
+++ client/index.js
@@ -1,29 +1,14 @@
-// async function query(data) {
-// 	const response = await fetch(
-// 		"https://api-inference.huggingface.co/models/bigscience/bloomz",
-// 		{
-// 			headers: { Authorization: "Bearer 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" },
-// 			method: "POST",
-// 			body: JSON.stringify(data),
-// 		}
-// 	);
-// 	const result = await response.json();
-// 	return result;
-// }
-
-// query({"inputs": "Can you please let us know more details about your "}).then((response) => {
-// 	console.log(JSON.stringify(response));
-// });
-
 //stuff to do with text bubbles ig
 let newUserArea;
 let newUserBubble;
 let newAiArea;
 let newAiBubble
 let aiResponse; 
+let loadInterval;
 
 //actrion listener for if user inputted stuff
 document.querySelector('#text').addEventListener('keypress', function (e) {
+	//checking when 'enter' key is pressed
 	if(e.key == 'Enter')
 	{
 		userEnter(document.getElementById('text').value);
@@ -34,7 +19,7 @@ document.querySelector('#text').addEventListener('keypress', function (e) {
 //Creates new user text bubble
 function userEnter(text)
 {
-	if((document.getElementById('text').value).charAt(0) != ' ')
+	if((document.getElementById('text').value).charAt(0) != ' ') //Making sure that the stuff being typed in isn't just white space
 	{
 		if(document.getElementById('text').value != '')
 		{
@@ -50,24 +35,75 @@ function userEnter(text)
 			//Reseting textbox
 			document.getElementById('text').value = null;
 
-			test(text);
-			//query(text);
+			//Running methods for next steps
+			getAIResponse(text);
+			aiBubble('')
+			gift()
+
 		}
 	}
 }
 
+function loader(element) {
+    element.textContent = 'thinking'
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === 'thinking....') {
+            element.textContent = 'thinking';
+        }
+    }, 300);
+}
 
 function aiBubble(response)
 {
-	//Creating new ai text bubble
-	newAiArea = document.createElement('div');
-	newAiBubble = document.createElement('div');
-	newAiArea.classList.add('aiArea');
-	newAiBubble.classList.add('aiBubble', 'textBubble');
-	newAiArea.appendChild(newAiBubble);
-	//setting text of user textbubble
-	newAiBubble.innerHTML = response;
-	document.getElementsByClassName('textArea')[0].appendChild(newAiArea)
+	if(response === '')
+	{
+		//Creating new ai text bubble
+		newAiArea = document.createElement('div');
+		newAiBubble = document.createElement('div');
+		newAiArea.classList.add('aiArea');
+		newAiBubble.classList.add('aiBubble', 'textBubble');
+		newAiArea.appendChild(newAiBubble);
+		//setting text of user textbubble
+		newAiBubble.innerHTML = response;
+		document.getElementsByClassName('textArea')[0].appendChild(newAiArea)
+		loader(newAiBubble);
+	}
+	else 
+	{
+		newAiBubble.innerHTML = response;
+		clearInterval(loadInterval)
+		if(response.length() > 200)
+			newAiBubble.style.padding = "2% 2% 2% 2%";
+		
+	}
+	
+		
+}
+
+
+//http request getting AI response
+async function getAIResponse(prompt)
+{
+	//Ai Response stored here
+    const response = await fetch("https://chatap-3obp.onrender.com/", {
+        method: "POST",
+        body: JSON.stringify({
+            message: prompt
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then((response) => response.json());
+
+	//Getting the AI response in the right format
+    const botResponse = response.bot.trim();
+    //console.log(botResponse);
+	aiBubble(botResponse)
 }
 
 function gift()
@@ -79,22 +115,3 @@ function gift()
 	if( randomNum === 69)
 		window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
-
-async function test(prompt)
-{
-    const response = await fetch("http://localhost:5000/", {
-        method: "POST",
-        body: JSON.stringify({
-            message: prompt
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    }).then((response) => response.json());
-
-    const botResponse = response.bot.trim();
-    console.log(botResponse);
-	aiBubble(botResponse)
-}
-
-
